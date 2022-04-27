b53e9c9bf7a6c42a547cfb49b1ebf4658b31ef57
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-04-27 19:03:40 +1000

    Generate a download table in README

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ accessible to anyone. This project started on 26/04/2022. Better late
 than never!
 
 See <https://github.com/asiripanich/bangkok-gtfs/tree/main/data/gtfs>.
+
+| time                | download_link                                                                                         |
+|:--------------------|:------------------------------------------------------------------------------------------------------|
+| 2022-04-26 18:19:11 | <https://github.com/asiripanich/bangkok-gtfs/tree/73348fb1866252f4d5bebeed3d2f7483d042e694/data/gtfs> |
--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 library(targets)
-
+library(tarchetypes)
 # Set target-specific options such as packages.
-tar_option_set(packages = c("here", "fs", "R.utils", "curl"))
+tar_option_set(packages = c("here", "fs", "R.utils", "curl", "rmarkdown", "gert", "dplyr"))
 source(here::here("R", "functions.R"))
 
 # End this file with a list of target objects.
@@ -11,5 +11,6 @@ list(
   tar_target(parking_url, get_parking_url(), format = "url"),
   tar_target(gtfs, download_file(gtfs_url)),
   tar_target(stop, download_file(stop_url)),
-  tar_target(parking, download_file(parking_url))
+  tar_target(parking, download_file(parking_url)),
+  tar_render(readme, here::here("README.Rmd"))
 )
